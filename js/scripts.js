36f77e567e8ad84085f1c190e7c44f7cc0e8a730
Armando Aguilar L <a.aguilar@gategeek.com>
2016-06-01 16:45:17 -0500

    Archivos inicial

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+function load_enbudo()
+{
+  $('#DEnvudo').show();
+  $("#DLista").hide();
+  $("#DCronograma").hide();
+
+}
+
+function load_lista()
+{
+  $('#DEnvudo').hide();
+  $("#DLista").show();
+  $("#DCronograma").hide();
+
+}
+
+function load_cronograma()
+{
+  $('#DEnvudo').hide();
+  $("#DLista").hide();
+  $("#DCronograma").show();
+}
