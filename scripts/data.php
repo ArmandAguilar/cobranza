33e07b34f1f6138865bf4d9366287e3d7ae325a2
Armando Aguilar L <a.aguilar@gategeek.com>
2016-06-02 17:43:02 -0500

    5:44

--- scripts/data.php
+++ scripts/data.php
@@ -1,12 +1,12 @@
 <?php
 include("../sis.php");
 include("$path/libs/conexion.php");
-include("$path/scripts/CDatos.php");
-$obj =  new panel();
+//include("$path/scripts/CDatos.php");
+//$obj =  new panel();
 
 switch ($_GET[v]) {
   case 'enbudo':
-            echo $obj->enbudo();
+            //echo $obj->enbudo();
     break;
   default:
     # code...
