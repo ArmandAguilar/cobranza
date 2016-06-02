bf7bbc016aefc430ba09484fd79de561928130f6
Armando Aguilar L <a.aguilar@gategeek.com>
2016-06-02 18:08:36 -0500

    6:08

--- scripts/CDatos.php
+++ scripts/CDatos.php
@@ -48,8 +48,9 @@ class panel extends poolConnecion
                     </div>
               </div>
           </div>
-      </div>
-      <!-- fila Contenido -->
+      </div>";
+
+      $row .= "<!-- fila Contenido -->
       <div class=\"row\">
         <div class=\"col-sm-2\">
                <div class=\"panel panel-pink panel-colorful\">
@@ -177,6 +178,29 @@ class panel extends poolConnecion
                                 </div>
                           </div>
                     </div>
+      </div>
+      <div class=\"row\">
+      <div class=\"col-sm-2\">
+             <div class=\"panel panel-pink panel-colorful\">
+                    <div class=\"pad-all media\">
+                      <div class=\"media-left\">
+                        <span class=\"icon-wrap icon-wrap-xs\">
+                          <i class=\"fa fa-dollar fa-fw fa-2x\"></i>
+                        </span>
+                      </div>
+                      <div class=\"media-body\">
+                        <p class=\"h3 text-thin media-heading\">7,428</p>
+                        <small class=\"text-uppercase\">Earning</small>
+                      </div>
+                    </div>
+                    <div class=\"progress progress-xs progress-dark-base mar-no\">
+                      <div style=\"width: 37.4%\" class=\"progress-bar progress-bar-light\" aria-valuemax=\"100\" aria-valuemin=\"0\" aria-valuenow=\"37.4\" role=\"progressbar\"></div>
+                    </div>
+                    <div class=\"pad-all text-right\">
+                      <small><span class=\"text-semibold\"><i class=\"fa fa-dollar fa-fw\"></i> 22,675</span> Total Earning</small>
+                    </div>
+              </div>
+        </div>
       </div>
       <!-- end fila Contenido -->";
                   return  $row;
