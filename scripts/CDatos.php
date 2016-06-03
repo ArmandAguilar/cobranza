5d9614c61e02da9bbc3a8fbdfa337592bcad2de7
Armando Aguilar L <a.aguilar@gategeek.com>
2016-06-02 22:58:00 -0500

    10:57pm

--- scripts/CDatos.php
+++ scripts/CDatos.php
@@ -18,7 +18,7 @@ class panel extends poolConnecion
                {
                  $row_col1.= "<div class=\"row\">
                                  <div class=\"col-lg-*\">
-                                   <div class=\"panel panel-gray panel-colorful\">
+                                   <div class=\"panel panel-purple panel-colorful\">
                                           <div class=\"pad-all media\">
                                             <div class=\"media-left\">
                                               <span class=\"icon-wrap icon-wrap-xs\">
