85930f0265457a0a13a2176046869da3b750b734
Armando Aguilar L <a.aguilar@gategeek.com>
2016-06-02 22:07:34 -0500

    10:07pm

--- scripts/CDatos.php
+++ scripts/CDatos.php
@@ -8,35 +8,34 @@ class panel extends poolConnecion
 
 
       $objPaso1 = new poolConnecion();
-      $Sql="Select * from RVEdoCtaGeneral";
+      $Sql="SELECT * FROM [SAP].[dbo].[RVEdoCtaGeneral]";
       $con=$objPaso1->ConexionSQLSAP();
       $RSet=$objPaso1->QuerySQLSAP($Sql,$con);
        while($fila=sqlsrv_fetch_array($RSet,SQLSRV_FETCH_ASSOC))
              {
-                   $row_col1.= <div class="row">
-                                   <div class="col-lg-*">
-                                     <div class="panel panel-pink panel-colorful">
-                                            <div class="pad-all media">
-                                              <div class="media-left">
-                                                <span class="icon-wrap icon-wrap-xs">
-                                                  <i class="fa fa-dollar fa-fw fa-2x"></i>
-                                                </span>
-                                              </div>
-                                              <div class="media-body">
-                                                <a href="detail_view.html"><p class="h3 text-thin media-heading">7,428</p></a>
-                                                <small class="text-uppercase">Earning</small>
-                                              </div>
-                                            </div>
-                                            <div class="progress progress-xs progress-dark-base mar-no">
-                                              <div style="width: 37.4%" class="progress-bar progress-bar-light" aria-valuemax="100" aria-valuemin="0" aria-valuenow="37.4" role="progressbar"></div>
-                                            </div>
-                                            <div class="pad-all text-right">
-                                              <small><span class="text-semibold"><i class="fa fa-dollar fa-fw"></i> 22,675</span> Total Earning</small>
-                                            </div>
-                                      </div>
-                                   </div>
-                               </div>";
-
+               $row_col1.= "<div class=\"row\">
+                               <div class=\"col-lg-*\">
+                                 <div class=\"panel panel-pink panel-colorful\">
+                                        <div class=\"pad-all media\">
+                                          <div class=\"media-left\">
+                                            <span class=\"icon-wrap icon-wrap-xs\">
+                                              <i class=\"fa fa-dollar fa-fw fa-2x\"></i>
+                                            </span>
+                                          </div>
+                                          <div class=\"media-body\">
+                                            <a href=\"detail_view.html\"><p class=\"h3 text-thin media-heading\">7,428</p></a>
+                                            <small class=\"text-uppercase\">Earning</small>
+                                          </div>
+                                        </div>
+                                        <div class=\"progress progress-xs progress-dark-base mar-no\">
+                                          <div style=\"width: 37.4%\" class=\"progress-bar progress-bar-light\" aria-valuemax=\"100\" aria-valuemin=\"0\" aria-valuenow=\"37.4\" role=\"progressbar\"></div>
+                                        </div>
+                                        <div class=\"pad-all text-right\">
+                                          <small><span class=\"text-semibold\"><i class=\"fa fa-dollar fa-fw\"></i> 22,675</span> Total Earning</small>
+                                        </div>
+                                  </div>
+                               </div>
+                           </div>";
 
               }
        $objPaso1->CerrarSQLSAP($RSet,$con);
