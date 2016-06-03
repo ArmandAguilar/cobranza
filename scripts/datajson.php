c037bd45d3e5e27a83f60f7a81c6297d6d91ada4
Armando Aguilar L <a.aguilar@gategeek.com>
2016-06-03 15:55:41 -0500

    3:55pm

--- scripts/datajson.php
+++ scripts/datajson.php
@@ -19,22 +19,22 @@ $objPaso2 = new poolConnecion();
 $Sql="SELECT [NumProyecto],[NomProyecto],[Estatus],[Vendedor],[Empresa] FROM [SAP].[dbo].[EstadoDeFacturasActivasxCobrar]";
 $con=$objPaso2->ConexionSQLSAP();
 $RSet=$objPaso2->QuerySQLSAP($Sql,$con);
- /*while($fila=sqlsrv_fetch_array($RSet,SQLSRV_FETCH_ASSOC))
+ while($fila=sqlsrv_fetch_array($RSet,SQLSRV_FETCH_ASSOC))
        {
 
           if(!empty($fila[NumProyecto]))
           {
               $contador++;
-             $strJson .= {
+             $strJson .= "{
                         "id": "$contador",
                         "noproyecto": "$fila[NumProyecto]",
                         "proyecto": "$fila[NomProyecto]",
                         "estado": "$fila[Estatus]",
                         "vendedor": "$fila[Vendedor]",
                         "empresa": "$fila[Empresa]"
-                      },
+                      },";
           }
-        }*/
+        }
 $strJson .= "]";
 echo $strJson;
 echo "2";
