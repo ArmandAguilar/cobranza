a4185ad713e4f02de379675f831d264bf41a8a8e
Armando Aguilar L <a.aguilar@gategeek.com>
2016-06-08 12:39:57 -0500

    12:39am 08/06/2016

--- scripts/CEmpresa.php
+++ scripts/CEmpresa.php
@@ -6,8 +6,8 @@ class empresa extends poolConnecion
   function detalle($id)
   {
             #Obtenemos el IdEmpresa de presupuestos
-            /*$objPresupuestos = new poolConnecion();
-            $Sql1="SELECT [IdEmpresa] FROM [SAP].[dbo].[presupuestos] Where NoProyecto='$id'";
+            $objPresupuestos = new poolConnecion();
+            $Sql1="SELECT IdEmpresa,Empresa,RazonSocial,RFC FROM empresas Where IdEmpresa='4144'";
             $con=$objPresupuestos->ConexionSQLSAP();
             $RSet=$objPresupuestos->QuerySQLSAP($Sql1,$con);
              while($fila=sqlsrv_fetch_array($RSet,SQLSRV_FETCH_ASSOC))
@@ -15,11 +15,11 @@ class empresa extends poolConnecion
 
                        $IdEmpresa = $fila[IdEmpresa];
                    }
-             $objPresupuestos->CerrarSQLSAP($RSet,$con);*/
+             $objPresupuestos->CerrarSQLSAP($RSet,$con);
 
-            $arr = array();
+            //$arr = array();
             #Detalle Empresa
-            $objEmpresa = new poolConnecion();
+            /*$objEmpresa = new poolConnecion();
             $Sql="SELECT IdEmpresa,Empresa,RazonSocial,RFC FROM empresas Where IdEmpresa='4144'";
             $con=$objEmpresa->ConexionSQLSAP();
             $RSet=$objEmpresa->QuerySQLSAP($Sql,$con);
@@ -32,8 +32,8 @@ class empresa extends poolConnecion
                                     );
                    }
              $objEmpresa->CerrarSQLSAP($RSet,$con);
-             //return json_encode($arr);
-             return "$fila[IdEmpresa]-$Sql";
+             //return json_encode($arr);*/
+             return "$fila[IdEmpresa] $Sql1";
   }
 }
 
