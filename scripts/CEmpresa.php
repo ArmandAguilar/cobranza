4edb8d55c6ce896cf5aa70c24a298b495d2ac4df
Armando Aguilar L <a.aguilar@gategeek.com>
2016-06-08 12:21:52 -0500

    12:21am 08/06/2016

--- scripts/CEmpresa.php
+++ scripts/CEmpresa.php
@@ -20,7 +20,7 @@ class empresa extends poolConnecion
             $arr = array();
             #Detalle Empresa
             $objEmpresa = new poolConnecion();
-            $Sql="SELECT Empresa,RazonSocial,RFC FROM [SAP].[dbo].[empresas] Where IdEmpresa='$IdEmpresa'";
+            $Sql="SELECT Empresa,RazonSocial,RFC FROM empresas Where IdEmpresa='$IdEmpresa'";
             $con=$objEmpresa->ConexionSQLSAP();
             $RSet=$objEmpresa->QuerySQLSAP($Sql,$con);
              while($fila=sqlsrv_fetch_array($RSet,SQLSRV_FETCH_ASSOC))
@@ -33,7 +33,7 @@ class empresa extends poolConnecion
                    }
              $objEmpresa->CerrarSQLSAP($RSet,$con);
              //return json_encode($arr);
-             return $fila[Empresa];
+             return "$fila[Empresa]-$Sql";
   }
 }
 
