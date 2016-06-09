ecbea83f3df6b4b713020baa9e4357d2190e20b2
Armando Aguilar L <a.aguilar@gategeek.com>
2016-06-09 17:39:00 -0500

    05:38 09/06/2016

--- detalle.php
+++ detalle.php
@@ -353,7 +353,7 @@ session_start();
 
                                 <!-- Timeline header -->
                                 <div class="timeline-header">
-                                  <div class="timeline-header-title bg-info">Now</div>
+                                  <div class="timeline-header-title bg-info">Ahora</div>
                                 </div>
 
                                 <div class="timeline-entry">
@@ -363,11 +363,21 @@ session_start();
                                     <div class="timeline-time">2 Hours ago</div>
                                   </div>
                                   <div class="timeline-label">
-                                    <h4 class="mar-no pad-btm"><a href="#" class="text-danger">Job Meeting</a></h4>
+                                    
                                     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt.</p>
                                   </div>
                                 </div>
+                                <div class="timeline-entry">
+                                  <div class="timeline-stat">
+                                    <div class="timeline-icon"><img src="https://lh3.googleusercontent.com/H5-LYU_c351B_sG6gZD4v6kGzsgBMkeN9xTMuOW2QO5oujgvi3ir8zDNTJX13oE-5XpKvx7aTw=w5120-h3200-rw-no" alt="Profile picture">
+                                    </div>
+                                    <div class="timeline-time">2 Hours ago</div>
+                                  </div>
+                                  <div class="timeline-label">
 
+                                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt.</p>
+                                  </div>
+                                </div>
 
 
                               </div>
