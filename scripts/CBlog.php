08707390c406deb79c8c7d6e891c605dea69e5d0
Armando Aguilar L <a.aguilar@gategeek.com>
2016-06-09 17:52:14 -0500

    05:59 09/06/2016

--- scripts/CBlog.php
+++ scripts/CBlog.php
@@ -28,7 +28,7 @@ class blog extends poolConnecion
 
       $TimeLine .= "<div class=\"timeline-entry\">
                         <div class=\"timeline-stat\">
-                          <div class=\"timeline-iconº"><img src=\"https://lh3.googleusercontent.com/H5-LYU_c351B_sG6gZD4v6kGzsgBMkeN9xTMuOW2QO5oujgvi3ir8zDNTJX13oE-5XpKvx7aTw=w5120-h3200-rw-no\" alt=\"Profile picture\">
+                          <div class=\"timeline-icon\"><img src=\"https://lh3.googleusercontent.com/H5-LYU_c351B_sG6gZD4v6kGzsgBMkeN9xTMuOW2QO5oujgvi3ir8zDNTJX13oE-5XpKvx7aTw=w5120-h3200-rw-no\" alt=\"Profile picture\">
                           </div>
                           <div class=\"timeline-time\">2 Hours ago</div>
                         </div>
@@ -36,7 +36,7 @@ class blog extends poolConnecion
                           <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt.</p>
                         </div>
                       </div>";
-        return $TimeLine;              
+        return $TimeLine;
     }
 }
 
