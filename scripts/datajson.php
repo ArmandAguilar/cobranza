2f7713298d02a0bcf63b95d7a81bffcdf80f922b
Armando Aguilar L <a.aguilar@gategeek.com>
2016-06-10 16:42:51 -0500

    04:33 10/06/2016

--- scripts/datajson.php
+++ scripts/datajson.php
@@ -3,7 +3,7 @@ include("../sis.php");
 include("$path/libs/conexion.php");
 
 
-/*$contador = 0;
+$contador = 0;
 $arr = array();
 $objPaso2 = new poolConnecion();
 $Sql="SELECT [NumProyecto],[NomProyecto],[Estatus],[Vendedor],[Empresa] FROM [SAP].[dbo].[EstadoDeFacturasActivasxCobrar]";
@@ -11,21 +11,25 @@ $con=$objPaso2->ConexionSQLSAP();
 $RSet=$objPaso2->QuerySQLSAP($Sql,$con);
  while($fila=sqlsrv_fetch_array($RSet,SQLSRV_FETCH_ASSOC))
        {
-
+         $NumProyecto = $fila[NumProyecto];
+         $NomProyecto = $fila[NomProyecto];
+         $Estatus = $fila[Estatus];
+         $Vendedor = $fila[Vendedor];
+         $Empresa = $fila[Empresa];
+         $IVA = $fila[IVA];
           if(!empty($fila[NumProyecto]))
           {
               $contador++;
               $arr[] = array('id' => $contador,
-                                'noproyecto' => $fila[NumProyecto],
-                                'proyecto' => $fila[NomProyecto],
-                                'estado' =>$fila[Estatus],
-                                'vendedor' =>$fila[Vendedor],
-                                'empresa' =>$fila[Empresa],
-                                'montoantesdeiva' =>$fila[Monto Antes de IVA],
-                                'iva' =>$fila[IVA]
+                                'noproyecto' => $NumProyecto,
+                                'proyecto' => $NomProyecto,
+                                'estado' =>$Estatus,
+                                'vendedor' =>$Vendedor,
+                                'empresa' =>$Empresa,
+                                'iva' =>$IVA
                     );
           }
         }
-echo json_encode($arr);*/
-echo "Goog";
+echo json_encode($arr);
+
 ?>
