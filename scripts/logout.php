152cd6271a67e7d0446fc6045580313f928c857c
Armando Aguilar L <a.aguilar@gategeek.com>
2016-06-10 12:20:49 -0500

    12:00 10/06/2016

---
+++ scripts/logout.php
@@ -0,0 +1,13 @@
+<?php
+
+ ?>
+<html>
+<head>
+<title>Cerrar session</title>
+</head>
+<body>
+<script>
+window.location.href='http://187.188.109.47:82/cobranza/login.php';
+</script>
+</body>
+</hmtl>
