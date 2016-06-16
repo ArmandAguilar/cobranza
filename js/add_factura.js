0add2eba630eb7a56b9a6b801c7fc3c230b850de
Armando Aguilar L <a.aguilar@gategeek.com>
2016-06-16 18:07:03 -0500

    06:07 16/06/2016pm

--- js/add_factura.js
+++ js/add_factura.js
@@ -1,27 +1,7 @@
 function sumar_iva()
 {
+  alert('hola');
   var iva = $("#cboIva").val();
   var newIva = iva * $("#txtCantidad").val();
    $("#txtImporteTotal").val(newIva);
-
-}
-function datos_empresa()
-{
-  var {idEmpresa = $("#cboEmpresa").val();
-  var losdatos = {idEmpresa:idEmpresa};
-    $.ajax({
-           		url:'./scripts/oper_add_factura.php?o=1',
-          		type:'POST',
-          		data:losdatos,
-          		success:function(data)
-          	         {
-          	            var dataJson = eval(data);
-                        $("#txtRasonSocial").val(dataJson[0].RazonSocial);
-                        $("#txtRFC").val(dataJson[0].RFC);
-                        $("#txtDir").val(dataJson[0].DireccionFiscal);
-          		       },
-          		error:function(req,e,er) {
-          			alert('error!' + er);
-          		}
-           });
 }
