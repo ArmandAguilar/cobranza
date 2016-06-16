5b9bb990f56726d692d857acd743f129caee33ec
Armando Aguilar L <a.aguilar@gategeek.com>
2016-06-16 11:47:19 -0500

    11:47 16/06/2016am

--- add_factura.php
+++ add_factura.php
@@ -152,37 +152,27 @@
 						</div>
 						<div class="panel-body">
                         <div class="row">
+																	<div class="col-md-1">
+																			<input type="text" class="form-control" placeholder="Factura"> -
+																	</div>
+																	<div class="col-md-1">
+																			<input type="text" class="form-control" placeholder="Numero"> -
+																	</div>
                                     <div class="col-md-1">
                                         <p class="text-bold">¿Quien Factura?</p>
                                     </div>
                                     <div class="col-md-2">
                                       <select class="selectpicker" title="Seleciona tipo de factura" data-width="100%">
-                                        <option>Family</option>
-                                        <option>Friend</option>
-                                        <option>Partner</option>
+                                        <option value="0">------</option>
+                                        <option value="C&I">C&I</option>
+                                        <option value="CeI">CeI</option>
                                       </select>
                                     </div>
                                     <div class="col-md-1">
                                         <p class="text-bold">Proyecto</p>
                                     </div>
                                     <div class="col-md-4">
-                                      <select data-placeholder="Proyecto" id="demo-chosen-select" tabindex="1">
-                                        <option value="United States">United States</option>
-                                        <option value="United Kingdom">United Kingdom</option>
-                                        <option value="Afghanistan">Afghanistan</option>
-                                        <option value="Aland Islands">Aland Islands</option>
-                                        <option value="Albania">Albania</option>
-                                        <option value="Algeria">Algeria</option>
-                                        <option value="American Samoa">American Samoa</option>
-                                        <option value="Andorra">Andorra</option>
-                                        <option value="Angola">Angola</option>
-                                        <option value="Anguilla">Anguilla</option>
-                                        <option value="Antarctica">Antarctica</option>
-                                        <option value="Antigua and Barbuda">Antigua and Barbuda</option>
-                                        <option value="Argentina">Argentina</option>
-                                        <option value="Armenia">Armenia</option>
-                                        <option value="Aruba">Aruba</option>
-                                      </select>
+                                      		<p class="text-bold"><?php echo $_POST[NoProyecto]; ?></p>
                                     </div>
                         </div>
                         <hr>
