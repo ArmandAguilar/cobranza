258877d5a9fa9e9c4eeaa1e5d3b2036a89865c67
Armando Aguilar L <a.aguilar@gategeek.com>
2016-06-17 17:10:22 -0500

    05:10pm

--- add_factura.php
+++ add_factura.php
@@ -351,13 +351,13 @@ $RSet=$objCboEmpresas->QuerySQLSAP($SqlEmpreas,$con);
               </div>
               <div class="panel-footer text-right">
                     <div class="row">
-                          <div class="col-md-7">
+                          <div class="col-md-9">
                                 <div class="alert alert-success fade in">
 										                      <button class="close" data-dismiss="alert"><span>×</span></button>
 										                      <strong>Well done!</strong> You successfully read this important alert message.
 									              </div>
                           </div>
-                          <div class="col-md-2">
+                          <div class="col-md-1">
                                 <button class="btn btn-primary" onclick="guardar_factura();">Crear Factura</button>
                           </div>
                     </div>
