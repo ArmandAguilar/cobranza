319e59cb9729b05003fbc7d8d3dba776e908339b
Armando Aguilar L <a.aguilar@gategeek.com>
2016-06-28 13:53:58 -0500

    28/06/2016 01:44pm

--- scripts/oper_relacionar_lider.php
+++ scripts/oper_relacionar_lider.php
@@ -1,13 +1,13 @@
 <?php
 include("../sis.php");
 include("$path/libs/conexion.php");
-include("$path/scripts/CFactuar.php");
-$objF =  new facturas();
+include("$path/scripts/CFactura.php");
+$objF = new facturas();
 switch ($_GET[o]) {
   case '1':
             $info->Id=$_POST[cboIdMaestro];
             $info->IdLider=$_POST[cboUsuario];
-            echo $objF ->relacionar_usuarios($info);
+            echo $objF->relacionar_usuarios($info);
     break;
 
     default:
