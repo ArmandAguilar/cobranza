8ae9a3ff09a56a0326522a4ee6f113e4c72efeb3
Armando Aguilar L <a.aguilar@gategeek.com>
2016-06-29 18:07:13 -0500

    29/06/07

--- panel.php
+++ panel.php
@@ -41,8 +41,10 @@ session_start();
 		 						window.location.href='logout.php'
 					</script>";
 	}
+  echo "Esto es el campo $_SESSION[CobranzaPerfil]";
   if(empty($_SESSION[CobranzaPerfil]))
   {
+
     echo "<script>
               window.location.href='logout.php'
         </script>";
