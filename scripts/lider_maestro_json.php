ab0e3896c481538096c184c3988bc9234bc2bb2e
Armando Aguilar L <a.aguilar@gategeek.com>
2016-07-12 15:45:31 -0500

    Modificaciones 12/07/2016 3:45pm

--- scripts/lider_maestro_json.php
+++ scripts/lider_maestro_json.php
@@ -8,9 +8,9 @@ $contador = 0;
 $arr = array();
 $objPaso2 = new poolConnecion();
 $Sql="SELECT
-      [SAP].[dbo].[RelacionMaestrosEsclavos].[NumMaestro],
-      [SAP].[dbo].[RelacionMaestrosEsclavos].[NumProyecto],
-      [SAP].[dbo].[Presupuestos].[Proyecto]
+      [SAP].[dbo].[RelacionMaestrosEsclavos].[NumMaestro] As NumMaestro,
+      [SAP].[dbo].[RelacionMaestrosEsclavos].[NumProyecto] As NumProyecto,
+      [SAP].[dbo].[Presupuestos].[Proyecto] As NomProyecto
       FROM
       [SAP].[dbo].[RelacionMaestrosEsclavos],
       [SAP].[dbo].[Presupuestos]
@@ -22,12 +22,16 @@ $RSet=$objPaso2->QuerySQLSAP($Sql,$con);
  while($fila=sqlsrv_fetch_array($RSet,SQLSRV_FETCH_ASSOC))
        {
 
+              $NumMaestro = $fila[NumMaestro];
+              $NumProyecto = $fila[NumProyecto];
+              $NomProyecto = $fila[NomProyecto];
+              $Lider = "Lider";
               $contador++;
               $arr[] = array('id' => "$contador",
                               'NumMaestro' => "$NumMaestro",
                               'NumProyecto' => "$NumProyecto",
                               'NomProyecto' => "$NomProyecto",
-                              'Lider' => "$Empresa"
+                              'Lider' => "$Lider"
                     );
 
         }
