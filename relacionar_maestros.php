fab2c4de529f216c103fe47ed4b6354f96d35a46
Armando Aguilar L <a.aguilar@gategeek.com>
2016-07-14 16:03:23 -0500

    14/07/2016 04:03

--- relacionar_maestros.php
+++ relacionar_maestros.php
@@ -193,7 +193,7 @@ include("$path/libs/conexion.php");
                          <div class="col-sm-1">
                             No. Maestro:
                          </div>
-                          <div class="col-lg-2">
+                          <div class="col-lg-1">
                             <div id="DivtxtNoMaestro" class="form-group has-feedback">
                                 <input type="text" id="txtNoMaestro" name="txtNoMaestro" class="form-control" placeholder="Numero maestro ...">
                           </div>
@@ -201,7 +201,7 @@ include("$path/libs/conexion.php");
                           <div class="col-sm-1">
                              Maestro:
                           </div>
-                           <div class="col-lg-6">
+                           <div class="col-lg-*">
                              <div id="DivtxtMaestro" class="form-group has-feedback">
                                  <input type="text" id="txtMaestro" name="txtMaestro" class="form-control" placeholder="Nombre maestro">
                            </div>
