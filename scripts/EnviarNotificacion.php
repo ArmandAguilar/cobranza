0639d3f0a2f74a2c673949da9ad6c3b7d682f1d7
Armando Aguilar L <a.aguilar@gategeek.com>
2016-07-21 17:00:24 -0500

    21/07/2016 05:00pm

--- scripts/EnviarNotificacion.php
+++ scripts/EnviarNotificacion.php
@@ -47,27 +47,17 @@ $RSet=$objTimeLine->QuerySQLSAP($Sql,$con);
           $Fecha = $fila[Fechas];
           $IdUsuario = $fila[IdUsuario];
           $Avatar = avatar($IdUsuario);
-          $TimeLine .= "<tr>
-            <td align=\"center\" valign=\"top\" style=\"border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%;\" class=\"list-item\"><table align=\"center\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\" style=\"width: inherit; margin: 0; padding: 0; border-collapse: collapse; border-spacing: 0;\">
-                <!-- LIST ITEM IMAGE -->
-                <!-- Image text color should be opposite to background color. Set your url, image src, alt and title. Alt text should fit the image size. Real image size should be x2 -->
-                <td align=\"left\" valign=\"top\" style=\"border-collapse: collapse; border-spacing: 0;
-                  padding-top: 30px;
-                  padding-right: 20px;\"><img
-                border=\"0\" vspace=\"0\" hspace=\"0\" style=\"padding: 0; margin: 0; outline: none; text-decoration: none; -ms-interpolation-mode: bicubic; border: none; display: block;
-                  color: #000000;\"
-                  src=\"$Avatar\"
-                  alt=\"H\" title=\"Highly compatible\"
-                  width=\"50\" height=\"50\"></td>
-                <!-- LIST ITEM TEXT -->
-                <!-- Set text color and font family (\"sans-serif\" or \"Georgia, serif\"). Duplicate all text styles in links, including line-height -->
-                <td align=\"left\" valign=\"top\" style=\"font-size: 17px; font-weight: 400; line-height: 160%; border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0;
-                  padding-top: 25px;
-                  color: #000000;
-                  font-family: sans-serif;\" class=\"paragraph\">
-                    <b style=\"color: #333333;\">$Usuario $Fecha</b><br/>
-                    $Mensaje
-                </td></tr>";
+          $TimeLine .= "
+                        <tr>
+                              <td align=\"left\" valign=\"top\" style=\"border-collapse: collapse; border-spacing: 0;padding-top: 30px;padding-right: 20px;\" width=\"20%\">
+                                  <img border=\"0\" vspace=\"0\" hspace=\"0\" style=\"padding: 0; margin: 0; outline: none; text-decoration: none; -ms-interpolation-mode: bicubic; border: none; display: block; color: #000000;\" src=\"$Avatar\" alt=\"H\" title=\"Highly compatible\"width=\"50\" height=\"50\">
+                              </td>
+                              <td align=\"left\" valign=\"top\" style=\"font-size: 17px; font-weight: 400; line-height: 160%; border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0;padding-top: 25px;color: #000000;font-family: sans-serif;\" class=\"paragraph\" width=\"80%\">
+                                  <b style=\"color: #333333;\">$Usuario $Fecha</b><br/>
+                                  $Mensaje
+                                  <hr color=\"#E0E0E0\" align=\"center\" width=\"100%\" size=\"1\" noshade style=\"margin: 0; padding: 0;\" />
+                              </td>
+                       </tr>";
        }
  $objTimeLine->CerrarSQLSAP($RSet,$con);
 
@@ -98,94 +88,50 @@ $RSet=$objTimeLine->QuerySQLSAP($Sql,$con);
  .footer a, .footer a:hover {
   color: #999999;
  }
-
     </style>
-
-  <!-- MESSAGE SUBJECT -->
   <title>Notificaciones Forta</title>
-
  </head>
+ <body topmargin=\"0\" rightmargin=\"0\" bottommargin=\"0\" leftmargin=\"0\" marginwidth=\"0\" marginheight=\"0\" width=\"100%\" style=\"border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; width: 100%; height: 100%; -webkit-font-smoothing: antialiased; text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%; line-height: 100%; background-color: #F0F0F0; color: #000000;\" bgcolor=\"#F0F0F0\" text=\"#000000\">
+ <table width=\"100%\" align=\"center\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" style=\"border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; width: 100%;\" class=\"background\">
+    <tr>
+        <td align=\"center\" valign=\"top\" style=\"border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0;\" bgcolor=\"#F0F0F0\">
+              <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" align=\"center\" width=\"560\" style=\"border-collapse: collapse; border-spacing: 0; padding: 0; width: inherit; max-width: 560px;\" class=\"wrapper\">
+                  <tr>
+                      <td align=\"center\" valign=\"top\" style=\"border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; padding-top: 20px; padding-bottom: 20px;\">
 
- <!-- BODY -->
- <!-- Set message background color (twice) and text color (twice) -->
- <body topmargin=\"0\" rightmargin=\"0\" bottommargin=\"0\" leftmargin=\"0\" marginwidth=\"0\" marginheight=\"0\" width=\"100%\" style=\"border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; width: 100%; height: 100%; -webkit-font-smoothing: antialiased; text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%; line-height: 100%;
-  background-color: #F0F0F0;
-  color: #000000;\"
-  bgcolor=\"#F0F0F0\"
-  text=\"#000000\">
- <!-- SECTION / BACKGROUND -->
- <!-- Set message background color one again -->
- <table width=\"100%\" align=\"center\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" style=\"border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; width: 100%;\" class=\"background\"><tr><td align=\"center\" valign=\"top\" style=\"border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0;\"
-  bgcolor=\"#F0F0F0\">
- <!-- WRAPPER -->
- <!-- Set wrapper width (twice) -->
- <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" align=\"center\"
-  width=\"560\" style=\"border-collapse: collapse; border-spacing: 0; padding: 0; width: inherit;
-  max-width: 560px;\" class=\"wrapper\">
+                      </td>
+                  </tr>
 
-  <tr>
-    <td align=\"center\" valign=\"top\" style=\"border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
-      padding-top: 20px;
-      padding-bottom: 20px;\">
-      <!-- PREHEADER -->
-      <!-- Set text color to background color -->
-    </td>
-  </tr>
+              </table>
+              <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" align=\"center\" bgcolor=\"#FFFFFF\" width=\"560\" style=\"border-collapse: collapse; border-spacing: 0; padding: 0; width: inherit;max-width: 560px;\" class=\"container\">
+                    <tr>
+                        <td align=\"center\" valign=\"top\" style=\"border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 24px; font-weight: bold; line-height: 130%;
+                          padding-top: 25px;
+                          color: #000000;
+                          font-family: sans-serif;\" class=\"header\">
 
- <!-- End of WRAPPER -->
+                        </td>
+                    </tr>
+              </table>
+              <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" align=\"center\" bgcolor=\"#FFFFFF\" width=\"560\" style=\"border-collapse: collapse; border-spacing: 0; padding: 0; width: inherit;max-width: 560px;\" class=\"container\">
+
+                    <tr>
+                      <td align=\"center\" valign=\"top\" style=\"border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
+                        padding-top: 25px;\" class=\"line\">
+
+                      </td>
+                      <td>
+                      </td>
+                    </tr>
+              </table>
+              <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" align=\"center\" bgcolor=\"#FFFFFF\" width=\"560\" style=\"border-collapse: collapse; border-spacing: 0; padding: 0; width: inherit;max-width: 560px;\" class=\"container\">
+                    <!-- Aqui Varible //-->
+                    $TimeLine
+            </table>
+
+        </td>
+    </tr>
  </table>
-
- <!-- WRAPPER / CONTEINER -->
- <!-- Set conteiner background color -->
- <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" align=\"center\"
-  bgcolor=\"#FFFFFF\"
-  width=\"560\" style=\"border-collapse: collapse; border-spacing: 0; padding: 0; width: inherit;
-  max-width: 560px;\" class=\"container\">
-
-  <!-- HEADER -->
-  <!-- Set text color and font family (\"sans-serif\" or \"Georgia, serif\") -->
-  <tr>
-    <td align=\"center\" valign=\"top\" style=\"border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 24px; font-weight: bold; line-height: 130%;
-      padding-top: 25px;
-      color: #000000;
-      font-family: sans-serif;\" class=\"header\">
-        $_POST[Factura]
-    </td>
-  </tr>
-  <!-- LINE -->
-  <!-- Set line color -->
-  <tr>
-    <td align=\"center\" valign=\"top\" style=\"border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
-      padding-top: 25px;\" class=\"line\"><hr
-      color=\"#E0E0E0\" align=\"center\" width=\"100%\" size=\"1\" noshade style=\"margin: 0; padding: 0;\" />
-    </td>
-  </tr>
-  $TimeLine
-    </table>
-  </td>
-  </tr>
-
-  <!-- LINE -->
-  <!-- Set line color -->
-  <tr>
-    <td align=\"center\" valign=\"top\" style=\"border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
-      padding-top: 25px;\" class=\"line\"><hr
-      color=\"#E0E0E0\" align=\"center\" width=\"100%\" size=\"1\" noshade style=\"margin: 0; padding: 0;\" />
-    </td>
-  </tr>
- <!-- End of WRAPPER -->
- </table>
-
-    </td>
-  </tr>
-
-
- <!-- End of WRAPPER -->
- </table>
-
- <!-- End of SECTION / BACKGROUND -->
- </td></tr></table>
-
  </body>
  </html>";
 
