0ebdf52372b8869a1db9cf61f6c5cf6b80c3d581
Armando Aguilar L <a.aguilar@gategeek.com>
2016-07-21 16:06:35 -0500

    21/07/2016 04:06pm

--- scripts/EnviarNotificacion.php
+++ scripts/EnviarNotificacion.php
@@ -71,7 +71,7 @@ $RSet=$objTimeLine->QuerySQLSAP($Sql,$con);
        }
  $objTimeLine->CerrarSQLSAP($RSet,$con);
 
- $mjs = "<html xmlns=\"http://www.w3.org/1999/xhtml\">
+ echo $mjs = "<html xmlns=\"http://www.w3.org/1999/xhtml\">
  <head>
   <meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\">
     <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0;\">
@@ -223,13 +223,13 @@ $RSet=$objTimeLine->QuerySQLSAP($Sql,$con);
  //convert HTML into a basic plain-text alternative body
  $mail->msgHTML($mjs);
  //send the message, check for errors
- if (!$mail->send())
+ /*if (!$mail->send())
   {
      echo "Mailer Error: " . $mail->ErrorInfo;
    }
 else
   {
      echo "Message sent!";
-   }
+   }*/
 
   ?>
