1243e3627427148b670253a9d709c8312d810009
Armando Aguilar L <a.aguilar@gategeek.com>
2016-07-21 14:35:50 -0500

    21/07/2016 02:35pm

--- scripts/EnviarNotificacion.php
+++ scripts/EnviarNotificacion.php
@@ -1,6 +1,7 @@
 <?php
 include("../sis.php");
 include("$path/libs/conexion.php");
+include("$path/libs/PHPMailer/PHPMailerAutoload.php");
 function avatar($Id)
 {
       #Obtenemos el avatar
@@ -57,13 +58,13 @@ $RSet=$objTimeLine->QuerySQLSAP($Sql,$con);
                 </td></tr>";
        }
  $objTimeLine->CerrarSQLSAP($RSet,$con);
- ?>
- <html xmlns="http://www.w3.org/1999/xhtml">
+
+ $mjs = "<html xmlns=\"http://www.w3.org/1999/xhtml\">
  <head>
- 	<meta http-equiv="content-type" content="text/html; charset=utf-8">
-   	<meta name="viewport" content="width=device-width, initial-scale=1.0;">
-  	<meta name="format-detection" content="telephone=no"/>
- 	<style>
+  <meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\">
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0;\">
+    <meta name=\"format-detection\" content=\"telephone=no\"/>
+  <style>
  /* Reset styles */
  body { margin: 0; padding: 0; min-width: 100%; width: 100% !important; height: 100% !important;}
  body, table, td, div, p, a { -webkit-font-smoothing: antialiased; text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%; line-height: 100%; }
@@ -75,99 +76,96 @@ $RSet=$objTimeLine->QuerySQLSAP($Sql,$con);
 
  /* Rounded corners for advanced mail clients only */
  @media all and (min-width: 560px) {
- 	.container { border-radius: 8px; -webkit-border-radius: 8px; -moz-border-radius: 8px; -khtml-border-radius: 8px;}
+  .container { border-radius: 8px; -webkit-border-radius: 8px; -moz-border-radius: 8px; -khtml-border-radius: 8px;}
  }
 
  /* Set color for auto links (addresses, dates, etc.) */
  a, a:hover {
- 	color: #127DB3;
+  color: #127DB3;
  }
  .footer a, .footer a:hover {
- 	color: #999999;
+  color: #999999;
  }
 
-  	</style>
+    </style>
 
- 	<!-- MESSAGE SUBJECT -->
- 	<title>Notificaciones Forta</title>
+  <!-- MESSAGE SUBJECT -->
+  <title>Notificaciones Forta</title>
 
  </head>
 
  <!-- BODY -->
  <!-- Set message background color (twice) and text color (twice) -->
- <body topmargin="0" rightmargin="0" bottommargin="0" leftmargin="0" marginwidth="0" marginheight="0" width="100%" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; width: 100%; height: 100%; -webkit-font-smoothing: antialiased; text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%; line-height: 100%;
- 	background-color: #F0F0F0;
- 	color: #000000;"
- 	bgcolor="#F0F0F0"
- 	text="#000000">
+ <body topmargin=\"0\" rightmargin=\"0\" bottommargin=\"0\" leftmargin=\"0\" marginwidth=\"0\" marginheight=\"0\" width=\"100%\" style=\"border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; width: 100%; height: 100%; -webkit-font-smoothing: antialiased; text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%; line-height: 100%;
+  background-color: #F0F0F0;
+  color: #000000;\"
+  bgcolor=\"#F0F0F0\"
+  text=\"#000000\">
  <!-- SECTION / BACKGROUND -->
  <!-- Set message background color one again -->
- <table width="100%" align="center" border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; width: 100%;" class="background"><tr><td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0;"
- 	bgcolor="#F0F0F0">
+ <table width=\"100%\" align=\"center\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" style=\"border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; width: 100%;\" class=\"background\"><tr><td align=\"center\" valign=\"top\" style=\"border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0;\"
+  bgcolor=\"#F0F0F0\">
  <!-- WRAPPER -->
  <!-- Set wrapper width (twice) -->
- <table border="0" cellpadding="0" cellspacing="0" align="center"
- 	width="560" style="border-collapse: collapse; border-spacing: 0; padding: 0; width: inherit;
- 	max-width: 560px;" class="wrapper">
+ <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" align=\"center\"
+  width=\"560\" style=\"border-collapse: collapse; border-spacing: 0; padding: 0; width: inherit;
+  max-width: 560px;\" class=\"wrapper\">
 
- 	<tr>
- 		<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
- 			padding-top: 20px;
- 			padding-bottom: 20px;">
- 			<!-- PREHEADER -->
- 			<!-- Set text color to background color -->
- 		</td>
- 	</tr>
+  <tr>
+    <td align=\"center\" valign=\"top\" style=\"border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
+      padding-top: 20px;
+      padding-bottom: 20px;\">
+      <!-- PREHEADER -->
+      <!-- Set text color to background color -->
+    </td>
+  </tr>
 
  <!-- End of WRAPPER -->
  </table>
 
  <!-- WRAPPER / CONTEINER -->
  <!-- Set conteiner background color -->
- <table border="0" cellpadding="0" cellspacing="0" align="center"
- 	bgcolor="#FFFFFF"
- 	width="560" style="border-collapse: collapse; border-spacing: 0; padding: 0; width: inherit;
- 	max-width: 560px;" class="container">
+ <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" align=\"center\"
+  bgcolor=\"#FFFFFF\"
+  width=\"560\" style=\"border-collapse: collapse; border-spacing: 0; padding: 0; width: inherit;
+  max-width: 560px;\" class=\"container\">
 
- 	<!-- HEADER -->
- 	<!-- Set text color and font family ("sans-serif" or "Georgia, serif") -->
- 	<tr>
- 		<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 24px; font-weight: bold; line-height: 130%;
- 			padding-top: 25px;
- 			color: #000000;
- 			font-family: sans-serif;" class="header">
- 				Proyecto
- 		</td>
- 	</tr>
- 	<!-- LINE -->
- 	<!-- Set line color -->
- 	<tr>
- 		<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
- 			padding-top: 25px;" class="line"><hr
- 			color="#E0E0E0" align="center" width="100%" size="1" noshade style="margin: 0; padding: 0;" />
- 		</td>
- 	</tr>
-  <?php echo $TimeLine; ?>
-
-
-
- 		</table>
+  <!-- HEADER -->
+  <!-- Set text color and font family (\"sans-serif\" or \"Georgia, serif\") -->
+  <tr>
+    <td align=\"center\" valign=\"top\" style=\"border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 24px; font-weight: bold; line-height: 130%;
+      padding-top: 25px;
+      color: #000000;
+      font-family: sans-serif;\" class=\"header\">
+        Proyecto
+    </td>
+  </tr>
+  <!-- LINE -->
+  <!-- Set line color -->
+  <tr>
+    <td align=\"center\" valign=\"top\" style=\"border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
+      padding-top: 25px;\" class=\"line\"><hr
+      color=\"#E0E0E0\" align=\"center\" width=\"100%\" size=\"1\" noshade style=\"margin: 0; padding: 0;\" />
+    </td>
+  </tr>
+  $TimeLine
+    </table>
   </td>
- 	</tr>
+  </tr>
 
- 	<!-- LINE -->
- 	<!-- Set line color -->
- 	<tr>
- 		<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
- 			padding-top: 25px;" class="line"><hr
- 			color="#E0E0E0" align="center" width="100%" size="1" noshade style="margin: 0; padding: 0;" />
- 		</td>
- 	</tr>
+  <!-- LINE -->
+  <!-- Set line color -->
+  <tr>
+    <td align=\"center\" valign=\"top\" style=\"border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
+      padding-top: 25px;\" class=\"line\"><hr
+      color=\"#E0E0E0\" align=\"center\" width=\"100%\" size=\"1\" noshade style=\"margin: 0; padding: 0;\" />
+    </td>
+  </tr>
  <!-- End of WRAPPER -->
  </table>
 
- 		</td>
- 	</tr>
+    </td>
+  </tr>
 
 
  <!-- End of WRAPPER -->
@@ -177,4 +175,49 @@ $RSet=$objTimeLine->QuerySQLSAP($Sql,$con);
  </td></tr></table>
 
  </body>
- </html>
+ </html>";
+ echo $mjs;
+/*
+ $mail = new PHPMailer;
+ //Tell PHPMailer to use SMTP
+ $mail->isSMTP();
+ //Enable SMTP debugging
+ // 0 = off (for production use)
+ // 1 = client messages
+ // 2 = client and server messages
+ $mail->SMTPDebug = 0;
+ //Ask for HTML-friendly debug output
+ $mail->Debugoutput = 'html';
+ //Set the hostname of the mail server
+ //Set the SMTP port number - likely to be 25, 465 or 587
+ $mail->SMTPSecure = 'tls';
+ $mail->Host = 'smtp.1and1.mx';
+ $mail->Port = 587;
+ //Whether to use SMTP authentication
+ $mail->SMTPAuth = true;
+ //Username to use for SMTP authentication
+ $mail->Username = "ventas@fortaingenieria.mx";
+ //Password to use for SMTP authentication
+ $mail->Password = "fortaMX010205**";
+ //Set who the message is to be sent from
+ $mail->setFrom('ventas@fortaingenieria.mx', 'Ventas');
+ //Set an alternative reply-to address
+ $mail->addReplyTo('ventas@fortaingenieria.mx', 'Ventas');
+ //Set who the message is to be sent to
+ $mail->addAddress('a.aguilar@fortaingenieria.com','Armando Aguilar L.');
+ //Set the subject line
+ $mail->Subject = 'Proyecto';
+ //Read an HTML message body from an external file, convert referenced images to embedded,
+ //convert HTML into a basic plain-text alternative body
+ $mail->msgHTML($msj);
+ //send the message, check for errors
+ if (!$mail->send())
+  {
+     echo "Mailer Error: " . $mail->ErrorInfo;
+   }
+else
+  {
+     echo "Message sent!";
+   }
+*/
+  ?>
